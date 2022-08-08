60bda8c2fdb4be9a92c9b1b50625cea3947e3d1c
MatiasVL <matias.vanlangenhove@gmail.com>
2022-08-08 09:19:08 +0200

    wrote tests

--- Java/src/test/java/com/gildedtros/GildedTrosTest.java
+++ Java/src/test/java/com/gildedtros/GildedTrosTest.java
@@ -5,13 +5,39 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedTrosTest {
+	Item[] items = new Item[] { new Item("Ring of Cleansening Code", 10, 20), new Item("Good Wine", 2, 0),
+			new Item("Elixir of the SOLID", 5, 7), new Item("B-DAWG Keychain", 0, 80),
+			new Item("B-DAWG Keychain", -1, 80), new Item("Backstage passes for Re:Factor", 15, 20),
+			new Item("Backstage passes for Re:Factor", 10, 49), new Item("Backstage passes for HAXX", 5, 49),
+			// TODO uncomment and fix test after implementing additional functionality
+//			 new Item("Duplicate Code", 3, 6),
+//			 new Item("Long Methods", 3, 6),
+//			 new Item("Ugly Variable Names", 3, 6)
+	};
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedTros app = new GildedTros(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
+	@Test
+	void testAllItemsForOnePeriod() {
+		Item[] items = new Item[] { new Item("Ring of Cleansening Code", 10, 20), new Item("Good Wine", 2, 0),
+				new Item("Elixir of the SOLID", 5, 7), new Item("B-DAWG Keychain", 0, 80),
+				new Item("B-DAWG Keychain", -1, 80), new Item("Backstage passes for Re:Factor", 15, 20),
+				new Item("Backstage passes for Re:Factor", 10, 49), new Item("Backstage passes for HAXX", 5, 49),
+				// these smelly items do not work properly yet
+				new Item("Duplicate Code", 3, 6), new Item("Long Methods", 3, 6),
+				new Item("Ugly Variable Names", 3, 6) };
+		GildedTros app = new GildedTros(items);
+		app.updateQuality();
+		assertEquals("Ring of Cleansening Code, 9, 19", app.items[0].toString());
+		assertEquals("Good Wine, 1, 1", app.items[1].toString());
+		assertEquals("Elixir of the SOLID, 4, 6", app.items[2].toString());
+		assertEquals("B-DAWG Keychain, 0, 80", app.items[3].toString());
+		assertEquals("B-DAWG Keychain, -1, 80", app.items[4].toString());
+		assertEquals("Backstage passes for Re:Factor, 14, 21", app.items[5].toString());
+		assertEquals("Backstage passes for Re:Factor, 9, 50", app.items[6].toString());
+		assertEquals("Backstage passes for HAXX, 4, 50", app.items[7].toString());
+		// TODO uncomment and fix test after implementing additional functionality
+//        assertEquals("fixme", app.items[8].toString());
+//        assertEquals("fixme", app.items[9].toString());
+//        assertEquals("fixme", app.items[10].toString());
+	}
 
 }
